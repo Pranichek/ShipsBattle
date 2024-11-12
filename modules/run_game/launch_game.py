38a065f282cc3dfacb5031e876dd6bbfbd9bc89a
admin <vovagrinchenko19@gmail.com>
2024-11-12 16:32:53 +0200

    add visual function to input text

--- modules/run_game/launch_game.py
+++ modules/run_game/launch_game.py
@@ -22,6 +22,7 @@ def start_game():
 
     run_game = True
     while run_game:
+        print(activa)
         server_module_screen.FPS.tick(60)
         server_module_screen.screen_server.fill((255 , 192 , 203))
         for event in pygame.event.get():
@@ -31,12 +32,13 @@ def start_game():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 #Якщо натиснуто всередині текстового поля
                 if text_box.collidepoint(event.pos):
+                    print(event.pos)
                     activa = True
                 #Якщо не натиснуто всередині текстового поля
                 else:
                     activa = False
 
-             #якщо натиснуто клавішу
+            #якщо натиснуто клавішу
             if event.type == pygame.KEYDOWN:
                 #чи текстове поле активне
                 if activa:
@@ -57,25 +59,29 @@ def start_game():
                             else:
                                 user_ip += event.unicode
 
-                        #якщо натиснта клавіша ENTER 
-                        if event.key == pygame.K_RETURN:
-                            print(user_ip)
-                            #якщо текстове поле не пусте та не знаходиться в списку користувачів
-                            if len(user_ip) > 0:
-                                if user_ip not in list_users:
-                                    #додаємо нового користувача до списку та джейсон файлу
-                                    list_users[user_ip] = {"points": 0}
-                                    write_json(filename = "data_base.json")
+                #якщо натиснта клавіша ENTER 
+                if event.key == pygame.K_RETURN:
+                    print(user_ip)
+                    #якщо текстове поле не пусте та не знаходиться в списку користувачів
+                    if len(user_ip) > 0:
+                        if user_ip not in list_users:
+                            #додаємо нового користувача до списку та джейсон файлу
+                            list_users[user_ip] = {"points": 0}
+                            write_json(filename = "data_base.json")
 
 
                             # if len(user_ip) > 0:
                             #     write_json(filename = "data_base.json", object_to_load = user_ip)
                         
         #якщо натиснуте текстове поле то воно червоне            
-        if activa:
+        if activa == True:
+            if user_ip == "nickname":
+                user_ip = ""
             color = pygame.Color("red")
         #якщо ні, то біле
-        else:
+        elif activa == False:
+            if len(user_ip) < 1 or user_ip == " ":
+                    user_ip = "nickname"
             color = pygame.Color("white")
         #малюємо текстове поле з заданим кольором та шириною
         pygame.draw.rect(surface = screen_server ,color = color ,rect =  text_box ,width = 4)
@@ -84,7 +90,7 @@ def start_game():
         #Відображаємо текст в текстовому полі , + 5 щоб текст не був прижатий до країв
         screen_server.blit(surf , (text_box.x + 5 , text_box.y + 5))
         #змінюємо ширину текстового поля в залежності від розміру тексту
-        #фугкція max робить так що текстове поле ніколи не буде меньшим ща 100 пікселів , але якщо текст буде стпноаитися більшим то текстове поле буде розтягуватись
+        #функція max робить так що текстове поле ніколи не буде меньшим за 100 пікселів , але якщо текст буде стпноаитися більшим то текстове поле буде розтягуватись
         text_box.w = max(100 , surf.get_width() + 10)
         #оновлюємо екран
         pygame.display.flip()
