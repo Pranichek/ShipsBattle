b71673a6b1316ece1bc5d1db1d744e0bcdcaabd3
admin <vovagrinchenko19@gmail.com>
2024-12-09 15:28:01 +0200

    create new folder json

--- modules/json_functions/write_json.py
+++ modules/json_functions/write_json.py
@@ -3,16 +3,18 @@ import os, json
 #створюємо словарь для збереження нікнеймів та балів користувачів
 list_users = {}
 
+
+#створюємо словарь для збереження статусу сервера
 list_server_status = {}
 
 
 #отримуємо дані з бази даних, яка знаходиться в папці static та у файлі data_base.json
-with open(file = os.path.abspath(__file__ + "/../../../static/utility.json")) as file:
+with open(file = os.path.abspath(__file__ + "/../../../static/json/utility.json")) as file:
     #загружаємо дані з json-файла в наш словник list_users
     list_server_status = json.load(file)
 
 #отримуємо дані з бази даних, яка знаходиться в папці static та у файлі data_base.json
-with open(file = os.path.abspath(__file__ + "/../../../static/data_base.json")) as file:
+with open(file = os.path.abspath(__file__ + "/../../../static/json/data_base.json")) as file:
     #загружаємо дані з json-файла в наш словник list_users
     list_users = json.load(file)
 
@@ -20,7 +22,7 @@ with open(file = os.path.abspath(__file__ + "/../../../static/data_base.json")) 
 #функція для збереження даних  із словаря list_users у потрібний файл, у нашому випадку filename = "data_base.json"
 def write_json(filename:str, object_dict: object):
     #Формуємо абсолютний шлях до файлу, який знаходиться в папці static
-    path_to_file = os.path.abspath(__file__ + f"/../../../static/{filename}")
+    path_to_file = os.path.abspath(__file__ + f"/../../../static/json/{filename}")
     #Відкриваємо файл у режимі запису ("w"), щоб зберегти в нього дані
     with open(path_to_file, "w") as file:
         json.dump(
