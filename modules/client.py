e36c760780f511e914c61e106c28c2958bf97d05
admin <vovagrinchenko19@gmail.com>
2025-01-15 10:28:57 +0200

    chanching widow to fight

--- modules/client.py
+++ modules/client.py
@@ -57,22 +57,26 @@ def start_client():
         write_json(filename= "utility.json" , object_dict = list_connect_status)
         # Бесконечный цикл для отправки и получения данных
         while True:
-            player_information = {
-                "turn": role,
-                'grid':list_grid,
-                "new_for_server" : server_module.enemy_matrix[0]
-            }
-            information_str = json.dumps(player_information)
-            client_socket.sendall(information_str.encode("utf-8"))
+            try:
+                data_ready = read_json(name_file = "status_connect_game.json")
+                status_ready_to_game = data_ready["status"] 
+                player_information = {
+                    "turn": role,
+                    "ready_to_fight": status_ready_to_game
+                }
+                information_str = json.dumps(player_information)
+                client_socket.sendall(information_str.encode("utf-8"))
 
-            data_enemy = client_socket.recv(1024).decode("utf-8")
-            server_module.enemy_data[0] = json.loads(data_enemy)
-            print(server_module.enemy_data[0]["grid"])
-            list_connect_status = {
-                "status": "connect"
-            }
-            write_json(filename= "utility.json" , object_dict = list_connect_status)
-            time.sleep(0.1)  
+                data_enemy = client_socket.recv(1024).decode("utf-8")
+                server_module.enemy_data[0] = json.loads(data_enemy)
+                list_connect_status = {
+                    "status": "connect"
+                }
+                write_json(filename= "utility.json" , object_dict = list_connect_status)
+                time.sleep(0.1)  
+            except:
+                print("Ошибка клиента:")
+                pass
     except Exception as e:
         print(f"Ошибка клиента: {e}")
 
