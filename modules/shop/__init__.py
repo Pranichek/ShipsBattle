22cf943437614bc24a5953cb552c7fc42ac0a41d
Lena Fedchenko <lenafedcenko98@gmail.com>
2025-01-16 21:32:17 +0200

    added coments for class ship

--- modules/shop/__init__.py
+++ modules/shop/__init__.py
@@ -1,8 +1,6 @@
 from .shop_image import shop_item
 from .shop_button import *
 from .text_shop import *
-<<<<<<< HEAD
 from .achivment import *
-=======
-from .tasks import *
->>>>>>> 5c0dea317ee606d7145d7a43b7b391fbe99bd221
+
+
