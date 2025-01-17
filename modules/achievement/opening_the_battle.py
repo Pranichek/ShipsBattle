486a37c41ad8a2e5f7c0783369348c09628ece2f
admin <vovagrinchenko19@gmail.com>
2025-01-17 23:05:44 +0200

    opening the battle

--- modules/achievement/opening_the_battle.py
+++ modules/achievement/opening_the_battle.py
@@ -8,17 +8,14 @@ def opening_the_battle(grid: list , enemy_grid: list):
     if player_ships[0] != 100:
         player_ships[0] = 0
         enemy_ships[0] = 0
-        count_enemy_zero = 0
         for row in range(len(grid)):
             for cell in range(len(grid[row])):
                 if grid[row][cell] in [1, 2, 3, 4]:
                     player_ships[0] += 1
                 if enemy_grid[row][cell] in [1, 2, 3, 4]:
                     enemy_ships[0] += 1
-                elif  enemy_grid[row][cell] == 0:
-                    count_enemy_zero += 1
 
-        if player_ships[0] == 20 and enemy_ships[0] <= 19 and count_enemy_zero != 100:
+        if player_ships[0] == 20 and enemy_ships[0] == 19:
             player_ships[0] = 100
             opening_the_battle_achievement.ACTIVE = True
             opening_battle_medal.ACTIVE = True
