79aa6ff1cbd7e811086c8c8cadd3518c676fb79b
admin <vovagrinchenko19@gmail.com>
2025-01-19 10:49:55 +0200

    add volume settings button

---
+++ modules/volume_settings/turn_off_volume.py
@@ -0,0 +1,19 @@
+import pygame
+from ..classes import Button
+from..classes.class_click import all_sounds
+pygame.init()
+
+def turn_off_volume_func():
+    pygame.mixer.music.set_volume(0)
+    for sound_effects in all_sounds:
+        sound_effects.set_volume(pygame.mixer.music.get_volume())
+
+off_sound_button = Button(
+    x = 205, 
+    y = 14, 
+    width = 74, 
+    height = 71, 
+    image_hover_path = "off_music.png", 
+    image_path = "off_music_hover.png", 
+    action = turn_off_volume_func
+    )
